22805fd60435e356dbdac530ee7b808a6035f129
Tomek <niewiemnicnic@onet.pl>
2018-10-17 18:41:05 +0200

    init

---
+++ src/main/java/pl/kurs/tomek/Cos_Tam/CosTamApplication.java
@@ -0,0 +1,12 @@
+package pl.kurs.tomek.Cos_Tam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosTamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosTamApplication.class, args);
+	}
+}
